87710faf7378dc9b9e4edae1bfc485e1cafb950d
gold <dgold@sjnma.org>
2017-09-18 13:34:37 -0500

    primitive variables

---
+++ math.py
@@ -0,0 +1,11 @@
+
+radius = 5
+diameter = 2* radius
+pi = 3.1415268987
+circumference =  pi * radius*2
+area = pi * radius * radius
+print("your circumeference is ")
+print(circumference)
+print("your area is")
+print(area)
+print("your radius was " + str(radius))
